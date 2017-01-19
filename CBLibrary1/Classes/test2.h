2141c229347bf846caca42911ca94b14902a4d80
com3611923 <caobincool@163.com>
2017-01-19 18:20:30 +0800

    add test2

---
+++ CBLibrary1/Classes/test2.h
@@ -0,0 +1,13 @@
+//
+//  test2.h
+//  Pods
+//
+//  Created by caobin on 17/1/19.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test2 : NSObject
+
+@end
---
+++ CBLibrary1/Classes/test2.m
@@ -0,0 +1,13 @@
+//
+//  test2.m
+//  Pods
+//
+//  Created by caobin on 17/1/19.
+//
+//
+
+#import "test2.h"
+
+@implementation test2
+
+@end
